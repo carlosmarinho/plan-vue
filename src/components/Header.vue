6731b3515aa55c65c20ae96887d438b0a0f0eaa9
Carlos Marinho <carluizfla@hotmail.com>
2019-08-07 16:53:31 -0300

    feito o header

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="pos-f-t">
+            <nav class="navbar navbar-dark bg-dark">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+            </nav>
+        </div>
+        <div class="container-fluid-nav text-center">
+            Planexo
+        </div>
+        <span class="navbar-text">
+            <a class="navbar-brand" href="#">Sair</a>
+        </span>
+    </nav>
+</template>
+
+<script>
+export default{
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+.navbar {
+    padding-top: 2px;
+    padding-bottom: 2px;
+}
+</style>
