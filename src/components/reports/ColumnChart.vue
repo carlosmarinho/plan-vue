aa7672042993cf4db0f153cd4651c8ec9e22f7ae
Carlos Marinho <carluizfla@hotmail.com>
2019-08-08 17:19:24 -0300

    passando parametros para o relatorio

---
+++ src/components/reports/ColumnChart.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-4">
+                <h3>{{ title }}</h3>
+            </div>
+            <div class="col-4"></div>
+            <div class="col-4">
+                <div class="text-right"><a href="#">Mais Detalhes</a></div>
+            </div>
+        </div>
+        <GChart
+            type="ColumnChart"
+            :data="chartData"
+            :options="chartOptions"
+        />
+    </div>
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+export default{
+    name: 'ColumnChart',
+    props: ['title', 'lastname', 'chartData', 'legend'],
+    data () {
+        return {
+            foo:'bar',
+            legendChart: legend,
+            chartOptions: {
+                chart: {
+                title: 'Company Performance',
+                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                },
+            }
+        }
+    },
+    components: {
+        GChart
+    }
+}
+
+</script>
+
+<style>
+</style>
